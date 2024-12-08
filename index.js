f1661569fd7974008873ec0a0617c29247c7cad2
yaredImam <yar1234yy@yahoo.com>
2024-12-08 09:37:44 -0800

    amazon api backend deploy

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const app = express();
+app.use(cors({ origin: true }));
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "success" });
+});
+app.post("/payment/create", async (req, res) => {
+  const total = parseInt(req.query.total);
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  }
+});
+app.listen(5000, (err) => {
+  if (err) throw err;
+  console.log("running at port http://localhost:5000");
+});
